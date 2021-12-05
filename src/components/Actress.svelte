140ed65b1beb728b9d907792dc12161a749dd2e2
zhen <zhen.yang@hotmail.fr>
2021-12-05 17:18:56 +0100

    maj

--- src/components/Actress.svelte
+++ src/components/Actress.svelte
@@ -7,7 +7,8 @@
     PageStore.set("actress");
     export let id;
     let actress = null;
-    let index = 0;
+    let photoindex = 0;
+    let photos_wrap;
     const unsubscribe = DataStore.subscribe(value => {
 		for(let entry of value["actresses"]){
             if(entry["id"] == id){
@@ -18,13 +19,32 @@
 	});
     onDestroy(unsubscribe);
 
-    function changeIndex(action=-1){
-        if(action == 1)index++;
-        if(action == 2)index--;
-        if(index >= actress["mkphotos"].length)index = 0;
-        if(index < 0)index = actress["mkphotos"].length - 1;
+    function changeIndex(action,index=null){
+        if(action == null){
+            photoindex = index;
+        }else{
+            if(action == 1)photoindex++;
+            if(action == 2)photoindex--;
+            if(photoindex >= actress["mkphotos"].length)photoindex = 0;
+            if(photoindex < 0)photoindex = actress["mkphotos"].length - 1;
+            window.scrollTo(photos_wrap.scrollLeft,0);
+        } 
     }
 	
+    //1:left; 2:right
+    function scrollCarousel(way){
+        let step = 73;
+        if(way == 1){
+            step = photos_wrap.scrollLeft - step;
+            if(step < 0)step = 0;
+        }
+        if(way == 2){
+            let max = photos_wrap.scrollWidth - photos_wrap.clientWidth;
+            step = photos_wrap.scrollLeft + step;
+            if(step > max)step = max;
+        }console.log(step)
+        photos_wrap.scrollTo(step, photos_wrap.scrollTop);
+    }
 </script>
 
 <div id="actress">
@@ -39,22 +59,39 @@
                             <h3 class="text-center">{actress.name}</h3>
                         </div>
                         <div class="col-6">
-                            <span class="pointer" on:click="{()=>changeIndex(2)}">
+                            <span class="pointer arrow" on:click="{()=>changeIndex(1)}">
                                 <ChevronLeft />
                                 Précédent
                             </span>
                         </div>
                         <div class="col-6 text-end">
-                            <span class="pointer" on:click="{()=>changeIndex(1)}">
+                            <span class="pointer arrow" on:click="{()=>changeIndex(2)}">
                                 Suivant
                                 <ChevronRight />
                             </span>
                         </div>
                         <div class="col-12 mt-2">
                             <div class="pe-4 ps-4">
-                                <img src="{actress['mkphotos'][index].photourl}" alt="" class="pointer" on:click="{()=>changeIndex(1)}">
+                                <img src="{actress['mkphotos'][photoindex].photourl}" alt="" class="pointer" on:click="{()=>changeIndex(1)}">
                             </div>
                         </div>
+                        {#if actress['mkphotos'].length > 1}
+                        <div class="col-12 mt-4">
+                            <div class="zcarousel">
+                                <div class="btn_arrow" on:click="{()=>scrollCarousel(1)}"><ChevronLeft /></div>
+                                <div class="photos_wrap" bind:this={photos_wrap}>
+                                    <div class="palette">
+                                        {#each actress["mkphotos"] as one,index(index)}
+                                        <div class="onephoto" class:active={photoindex==index} on:click="{()=>changeIndex(null,index)}">
+                                            <img src="{one.photourl}" alt="">
+                                        </div>
+                                        {/each}
+                                    </div>
+                                </div>
+                                <div class="btn_arrow" on:click="{()=>scrollCarousel(2)}"><ChevronRight /></div>
+                            </div>
+                        </div>
+                        {/if}
                     </div>
                     {/if}
                 </div>
@@ -79,5 +116,55 @@
 </div>
 
 <style>
-	
+	.arrow{
+        padding:5px;
+    }
+    .arrow:hover{
+        background-color: #f8b0c8;
+        color:white;
+        border-radius: 2px;
+    }
+
+    .zcarousel{
+        width:100%;
+        display:flex;
+    }
+    .zcarousel .btn_arrow{
+        width:30px;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        background-color:#f8b0c8;
+        color:white;
+        cursor: pointer;
+        height:120px;
+    }
+    .zcarousel .photos_wrap{
+        width:calc(100% - 60px);
+        display:flex;
+        align-items: center;
+        overflow: hidden;
+    }
+    .zcarousel .photos_wrap .palette{
+        /* width:9999px; */
+        display:flex;
+        align-items: center;
+    }
+    .zcarousel .photos_wrap .onephoto{
+        width:70px;
+        height:80px;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        border:3px solid transparent;
+        cursor: pointer;
+    }
+    .zcarousel .photos_wrap .onephoto.active{
+        border-color:#FF88af;
+    }
+    .zcarousel .photos_wrap .onephoto img{
+        width:100%;
+        height:auto;
+    }
 </style>
